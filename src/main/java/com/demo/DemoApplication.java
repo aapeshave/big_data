bff5ea9c4836678a1efda8630b5ef280ca448e53
AJinkya Peshave <ajinkya.peshave@sophos.com>
2016-10-06 19:25:44 -0400

    Initial Commit

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,10 +1,17 @@
 package com.demo;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.SpringTemplateLoader;
+
+import java.io.InputStream;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -17,5 +24,13 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @Bean
+    public JSONObject getPersonSchema(){
+        InputStream stream =  SpringApplication.class.getResourceAsStream("resources/person-schema.json");
+        String strSteam = stream.toString();
+                return new JSONObject();
     }
 }
--- src/main/java/com/demo/controller/PersonController.java
+++ src/main/java/com/demo/controller/PersonController.java
@@ -2,17 +2,30 @@ package com.demo.controller;
 
 
 import com.demo.service.PersonService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonSimpleJsonParser;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 
 @RestController
 public class PersonController {
+
+
+    private static final String PERSON_SCHEMA = "person-schema.json";
 
     @Autowired
     PersonService personService;
@@ -30,6 +43,21 @@ public class PersonController {
     public String addPerson(@RequestBody String body)
     {
         personService.addPerson(body);
+        isPersonSchemaValidated(body, "Ajinkya");
         return body;
+    }
+
+    private Boolean isPersonSchemaValidated(String jsonData, String jsonSchema)
+    {
+        JSONParser parser = new JSONParser();
+
+        try {
+            String someList = (String) parser.parse(new FileReader("person-schema.json"));
+            System.out.println("Printing String:" + someList);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return Boolean.FALSE;
     }
 }
