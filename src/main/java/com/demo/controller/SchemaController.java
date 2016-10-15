e77f63ea2ee12eaae556cede7bdc42c7c0c2b1ef
aapeshave <peshave.aj@husky.neu.edu>
2016-10-15 09:44:08 -0400

    Working schema GET,POST

--- src/main/java/com/demo/controller/SchemaController.java
+++ src/main/java/com/demo/controller/SchemaController.java
@@ -3,14 +3,16 @@ package com.demo.controller;
 import com.demo.service.SchemaService;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.commons.lang3.StringUtils;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import java.util.Map;
 
@@ -22,28 +24,29 @@ public class SchemaController {
 
     @POST
     @RequestMapping("/")
-    public String addSchema(@RequestBody String entity)
-    {
-        if (entity != null)
-        {
+    public String addSchema(@RequestBody String entity) {
+        if (entity != null) {
             JSONParser parser = new JSONParser();
             try {
                 Map<String, Object> object = (Map<String, Object>) parser.parse(entity);
-                int i=0;
                 String objectName = (String) object.get("objectName");
-                String jsonSchema = (String) object.get("jsonSchema").toString();
+                return schemaService.addSchemaToRedis(entity, objectName);
+
             } catch (ParseException e) {
                 e.printStackTrace();
             }
-            //return schemaService.addSchemaToRedis(entity.jsonSchema, entity.objectName.toString());
         }
         return null;
     }
 
-    public static class SchemaEntity
+    @GET
+    @RequestMapping("/{uuid}")
+    public String getSchema(@PathVariable("uuid") String schemaPath)
     {
-        @JsonProperty String jsonSchema;
-        @JsonProperty JSONPObject objectName;
+        if (!StringUtils.isBlank(schemaPath))
+        {
+            return schemaService.getSchemaFromRedis(schemaPath);
+        }
+        return null;
     }
-
 }
