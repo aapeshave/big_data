3611f359696ed339a5eb8f8a36ee0131f86194ab
AJinkya Peshave <ajinkya.peshave@sophos.com>
2016-11-09 14:57:59 -0500

    Deprecating the person controller. Adding swagger annotations on PersonControllerV1

--- src/main/java/com/demo/controller/PersonControllerV1.java
+++ src/main/java/com/demo/controller/PersonControllerV1.java
@@ -3,12 +3,10 @@ package com.demo.controller;
 import com.demo.service.PersonService;
 import com.demo.service.SchemaService;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import io.swagger.annotations.*;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
@@ -20,6 +18,7 @@ import java.io.IOException;
  */
 
 @RestController
+@Api(description = "New Api to do CRUD operations on a person with or without user account.")
 @RequestMapping("/v1/person")
 public class PersonControllerV1 {
 
@@ -31,8 +30,20 @@ public class PersonControllerV1 {
 
     private static final String SCHEMA_LOCATION = "SCHEMA__person";
 
-    @POST
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ApiOperation(value = "Create a person",response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 403,
+                    message = "json schema not validated",
+                    responseHeaders = @ResponseHeader(
+                            name = "BadRequest",
+                            description = "valid json schema should be provided")),
+            @ApiResponse(code = 500,
+                    message = "Internal Server Error",
+                    responseHeaders = @ResponseHeader(
+                            name = "GENERAL_ERROR",
+                            description = "unhandled exception occured"))
+    })
     public String addPerson(@RequestBody String body, HttpServletResponse response) throws IOException {
         if (!(body.isEmpty())) {
             try {
@@ -51,8 +62,15 @@ public class PersonControllerV1 {
         return null;
     }
 
-    @GET
-    @RequestMapping("/{uuid}")
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
+    @ApiOperation(value = "Get a person from database",response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 500,
+                    message = "Internal Server Error",
+                    responseHeaders = @ResponseHeader(
+                            name = "GENERAL_ERROR",
+                            description = "unhandled exception occured"))
+    })
     public String getPerson(@PathVariable("uuid") String uid) {
         if (!StringUtils.isBlank(uid)) {
             return _personService.v1GetPerson(uid);
